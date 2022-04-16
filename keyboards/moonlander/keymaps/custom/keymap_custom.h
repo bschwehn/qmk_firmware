d19f4edfbb3c47fae97fe53385d151188bd0fdfa
Benjamin Schwehn <bschwehn@gmail.com>
2022-04-16 14:37:55 +0200

    automatic merge from configurator

--- keyboards/moonlander/keymaps/custom/keymap_custom.h
+++ keyboards/moonlander/keymaps/custom/keymap_custom.h
@@ -6,20 +6,20 @@ enum custom_combo {
 
 bool custom_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_caps_word(keycode, record)) { return false; }
+    uint8_t mod_state = get_mods();
     switch (keycode) {
     case CUSTOM_QU:
-        static bool isShifted;
+        static bool is_shifted;
         if (record->event.pressed) {
-            isShifted = get_mods() & MOD_MASK_SHIFT;
-            if (isShifted) {
+            is_shifted = get_mods() & MOD_MASK_SHIFT;
+            if (is_shifted) {
                 del_mods(MOD_MASK_SHIFT);
                 SEND_STRING("Qu");
+                set_mods(mod_state);
             } else {
                 SEND_STRING("qu");
             }
         } else {
-            if (isShifted) {
-            }
         }
         return false;
     }
