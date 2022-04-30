c50f871d2823f8246da99d1bc0182d350c84aad8
Benjamin Schwehn <bschwehn@gmail.com>
2022-04-30 18:16:09 +0200

    automatic merge from configurator

--- keyboards/moonlander/keymaps/custom/custom.c
+++ keyboards/moonlander/keymaps/custom/custom.c
@@ -114,9 +114,11 @@ combo_t key_combos[] = {
 
 };
 const key_override_t apo_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_QUOTE);
+const key_override_t dash_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, UK_MINS);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
 	&apo_key_override,
+	&dash_key_override,
 	NULL // Null terminate the array of overrides!
 };
