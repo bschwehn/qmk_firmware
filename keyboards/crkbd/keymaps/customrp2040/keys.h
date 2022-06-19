ba0082ffb47e81a3caccb32be432ba81a3926576
Benjamin Schwehn <bschwehn@gmail.com>
2022-06-19 23:20:05 +0200

    adding fifi with kb2040 keymap, also with repeat keys

---
+++ keyboards/crkbd/keymaps/customrp2040/keys.h
@@ -0,0 +1,16 @@
+enum custom_combo {
+    CUSTOM_QU = SAFE_RANGE + 5,
+    CUSTOM_AUML,
+    CUSTOM_UUML,
+    CUSTOM_OUML,
+    CUSTOM_BSPACE,
+    CUSTOM_REPEAT,
+    CUSTOM_END_CAPS,
+    CUSTOM_ESC,
+    CUSTOM_ENTER,
+    CUSTOM_LAST
+};
+
+enum custom_ext {
+    LLOCK = CUSTOM_LAST,
+};
