c2d7afc102afa7db04f773ccb698a6a2a37cfba3
Benjamin Schwehn <bschwehn@gmail.com>
2026-04-18 15:12:14 +0200

    new charybdis map, upstream QMK

---
+++ apply_charybdis_old.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+copy_firmware() {
+    cp bastardkb_charybdis_4x6_v2_splinky_3_custom.uf2 /media/RPI-RP2
+}
+#make crkbd:customrp2040
+qmk compile -kb bastardkb/charybdis/4x6/v2/splinky_3 -km custom
+spacefm&
+until copy_firmware; do
+    if [ $? -eq 5 ]; then
+        echo "Error was 5"
+    else
+        echo "Error was not 5"
+    fi
+    sleep 1
+    # potentially, other code follows...
+done
+echo "second side"
+sleep 10
+spacefm&
+until copy_firmware; do
+    if [ $? -eq 5 ]; then
+        echo "Error was 5"
+    else
+        echo "Error was not 5"
+    fi
+    sleep 1
+    # potentially, other code follows...
+done
+
+cp bastardkb_charybdis_4x6_v2_splinky_3_custom.uf2 "../firmware_backups/bastardkb_charybdis_4x6_v2_splinky_3_custom.uf2.$(date -Iseconds)"
+ls ../firmware_backups
